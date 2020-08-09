5dc7479e37a873a7488e3ed2583d654b1501c795
akshatchaturvedi888 <akshatchaturvedi884@gmail.com>
2020-08-09 22:26:04 +0530

    Initial commit

---
+++ clacul.js
@@ -0,0 +1,45 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res) {
+    
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+
+var result = num1 + num2;
+ 
+
+    res.send("the result of our calci is: " + result);
+ 
+});
+
+
+
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiClac.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    
+    var bmi = weight/(height*height);
+
+    res.send("Your BMI Calculation is : " +bmi);
+
+});
+
+app.listen(3000, function () {
+    console.log("Server is running");
+});
+
